1cd1f1b3f9e4f56582f80690f47659dc08156e7b
Jürmo Mehine <jyrmo.mehine@gmail.com>
2018-11-03 15:10:08 +0200

    Match text angle animation.

--- src/gameObjects/MatchText.js
+++ src/gameObjects/MatchText.js
@@ -1,3 +1,5 @@
+import * as lodash from 'lodash'
+
 import {default as Timer} from '../Timer'
 import {lerp} from '../lerp'
 
@@ -11,6 +13,7 @@ class MatchText {
     // TODO: placement
     // TODO: better hiding
     this.text = this.scene.add.bitmapText(360, 360, 'calibri', '', 32)
+    this.text.depth = 10000
     // TODO: different tint
     this.text.tint = 0xf05050
 
@@ -22,6 +25,9 @@ class MatchText {
     this.text.text = text
     this.isDisplaying = true
     this.text.fontSize = origSize
+    this.origAngle = lodash.random(-90, 0)
+    this.destAngle = lodash.random(1, 90)
+    this.text.angle = this.origAngle
     this.text.visible = true
     this.displayTimer.start()
   }
@@ -35,10 +41,10 @@ class MatchText {
         this.isDisplaying = false
         this.text.visible = false
       } else {
-        // TODO: animate angle
         // TODO: animate opacity
         const fraction = elapsed / displayDuration
         this.text.fontSize = lerp(origSize, destSize, fraction)
+        this.text.angle = lerp(this.origAngle, this.destAngle, fraction)
       }
     }
   }
