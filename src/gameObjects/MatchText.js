c268fa01a1d5cbef3fd9e0b35d123940b5505f85
Jürmo Mehine <jyrmo.mehine@gmail.com>
2018-11-03 15:26:51 +0200

    Match text opacity animation.

--- src/gameObjects/MatchText.js
+++ src/gameObjects/MatchText.js
@@ -3,15 +3,17 @@ import * as lodash from 'lodash'
 import {default as Timer} from '../Timer'
 import {lerp} from '../lerp'
 
-const displayDuration = 500
+const displayDuration = 1000
+const alphaMidPoint = 700
 const origSize = 32
 const destSize = 64
+const origAlpha = 0
+const destAlpha = 1
 
 class MatchText {
   constructor(scene) {
     this.scene = scene
     // TODO: placement
-    // TODO: better hiding
     this.text = this.scene.add.bitmapText(360, 360, 'calibri', '', 32)
     this.text.depth = 10000
     // TODO: different tint
@@ -25,9 +27,11 @@ class MatchText {
     this.text.text = text
     this.isDisplaying = true
     this.text.fontSize = origSize
-    this.origAngle = lodash.random(-90, 0)
-    this.destAngle = lodash.random(1, 90)
+    this.origAngle = lodash.random(-20, 0)
+    this.destAngle = lodash.random(1, 20)
+    // TODO: anchor (angle pivot) is off
     this.text.angle = this.origAngle
+    this.text.alpha = origAlpha
     this.text.visible = true
     this.displayTimer.start()
   }
@@ -41,11 +45,21 @@ class MatchText {
         this.isDisplaying = false
         this.text.visible = false
       } else {
-        // TODO: animate opacity
         const fraction = elapsed / displayDuration
         this.text.fontSize = lerp(origSize, destSize, fraction)
         this.text.angle = lerp(this.origAngle, this.destAngle, fraction)
+        this.updateAlpha(elapsed)
       }
+    }
+  }
+  
+  updateAlpha(elapsed) {
+    if (elapsed < alphaMidPoint) {
+      const fraction = elapsed / alphaMidPoint
+      this.text.alpha = lerp(origAlpha, destAlpha, fraction)
+    } else {
+      const fraction = (elapsed - alphaMidPoint) / (displayDuration - alphaMidPoint)
+      this.text.alpha = lerp(destAlpha, origAlpha, fraction)
     }
   }
 }
