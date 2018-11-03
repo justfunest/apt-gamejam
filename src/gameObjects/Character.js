70db56edc9cc5f2306e3fe3340b88d5453ab8dd3
imre <imre.radusepp@ridango.com>
2018-11-03 13:29:41 +0200

    healthbar changes

--- src/gameObjects/Character.js
+++ src/gameObjects/Character.js
@@ -1,37 +1,30 @@
-
+import HealthBar from '../gameObjects/HealthBar'
 
 export const INITIAL_STATE = {
     soberness: {
         max : 500,
         current: 500,
-        decayRate: 0.1
+        decayRate: 0.05
     },
     position: {
-        x: 700,
-        y: 300
+        x: 900,
+        y: 400
     },
     alive : true,
     sprites: {
         init: 'character'
     }
 };
-import HealthBar from '../gameObjects/HealthBar'
+
 class Character extends Phaser.GameObjects.Sprite {
     constructor(scene, type, state = INITIAL_STATE) {
         super(scene, type);
         this.state = state;
         this.name = 'character';
         this.sprite = scene.add.sprite(this.state.position.x, this.state.position.y, 'character');
-
-        this.masksprite = scene.add.image(this.state.position.x, this.state.position.y, 'character').setVisible(false);
-        this.mask = this.masksprite.createBitmapMask();
-        this.sprite.setMask(this.mask);
-
-        this.bar = new HealthBar(scene, this.state.position.x + this.sprite.width/2 + 20, this.state.position.y - this.sprite.height/2, this.state.soberness.max, this.state.soberness.current)
-        //0xF0AF6B
-        let rect = new Phaser.Geom.Rectangle(this.state.position.x + this.sprite.width/2 + 20, this.state.position.y - this.sprite.height/2, 40, this.sprite.height);
-        let graphics = scene.add.graphics({ fillStyle: { color: 0x66ff66 } });
-        graphics.fillRectShape(rect);
+        let healthXPos = this.state.position.x;
+        let healthYPos = this.state.position.y - this.sprite.height/2 - 50;
+        this.healthBar = new HealthBar(scene, healthXPos, healthYPos, this.state.soberness.max, this.state.soberness.current)
 
     }
 
@@ -57,10 +50,7 @@ class Character extends Phaser.GameObjects.Sprite {
             this.getSober();
             this.checkIsAlive();
         }
-        this.masksprite.x += 0.1
-        this.bar.update(this.state.soberness.current);
-
-        //console.log(this.state.soberness.current)
+        this.healthBar.update(this.state.soberness.current);
     }
 
 }
--- src/gameObjects/HealthBar.js
+++ src/gameObjects/HealthBar.js
@@ -1,17 +1,24 @@
 class HealthBar {
     constructor(scene, x, y, max = 100, current = 100) {
-        this.bar = scene.add.sprite(x, y, 'healthbar').setScale(0.3, 0.3)
-        this.maskbar = scene.add.image(x, y, 'healthbar').setScale(0.3,0.3).setVisible(false);
+        this.bar = scene.add.sprite(x, y, 'healthbar').setScale(0.3, 0.2).setZ(1);
+        this.maskbar = scene.add.image(x, y, 'healthbar').setScale(0.3,0.2).setVisible(false);
         this.mask = this.maskbar.createBitmapMask();
         this.startX = this.maskbar.x;
         this.bar.setMask(this.mask);
         this.max = max;
         this.current = current;
+        this.text = scene.add.text(x, y, this.getText());
+        this.text.x -= this.text.width/2;
+        this.text.y -= this.text.height/2;
 
     }
 
+    getText() {
+        return Math.floor(this.current) + '/' + this.max;
+    }
     update(current) {
         this.current = current;
+        this.text.setText(this.getText());
         this.maskbar.x = this.startX - this.bar.width + (this.current / this.max * this.bar.width);
     }
 
